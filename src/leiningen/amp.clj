4ef464797dac5e3afe13be771b9559a7cea75fbd
Peter Monks <pmonks@gmail.com>
2014-05-16 12:48:13 -0700

    Better task checking

--- src/leiningen/amp.clj
+++ src/leiningen/amp.clj
@@ -21,8 +21,8 @@
 
    Usage:
 
-     lein amp package [<options>] [<path>]
-     lein amp deploy [<options>]
+     lein amp package [<options>]
+     lein amp deploy [<options>] [<path-to-alfresco>]
 
    Tasks:
 
@@ -32,22 +32,6 @@
    Commandline Options:
 
      ####TODO - FIX THIS
-
-     :aggressive          Check all available repositories (= Do not stop after first artifact match).
-     :all                 Check Dependencies and Plugins.
-     :allow-all           Allow SNAPSHOT and qualified versions to be reported as new.
-     :allow-qualified     Allow '*-alpha*' versions & co. to be reported as new.
-     :allow-snapshots     Allow '*-SNAPSHOT' versions to be reported as new.
-     :check-clojure       Include Clojure (org.clojure/clojure) in checks.
-     :dependencies        Check Dependencies. (default)
-     :interactive         Run 'upgrade' in interactive mode, prompting whether to apply changes.
-     :no-colors           Disable colorized output.
-     :no-profiles         Do not check Dependencies/Plugins in Profiles.
-     :no-tests            Do not run tests after upgrading a project.
-     :overwrite-backup    Do not prompt if a backup file exists when upgrading a project.
-     :plugins             Check Plugins.
-     :print               Print result of 'upgrade' task instead of writing it to 'project.clj'.
-     :recursive           Perform recursive 'check' or 'upgrade'.
   "
   [project & args]
   (let [^String t (when-let [^String t (first args)]
@@ -56,4 +40,4 @@
         args      (if run-task! (rest args) args)]
     (if run-task!
       (run-task! project args)
-      (println "Unknown task" t))))
+      (println "Unknown task" (if (nil? t) "<no task specified>" t)))))
