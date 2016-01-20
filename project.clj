cfaa3f4aa1ce69fff3a77253000379b45b88e1fd
Peter Monks <pmonks@gmail.com>
2016-01-19 23:32:58 -0800

    :arrow_up: Upgraded dependencies

--- project.clj
+++ project.clj
@@ -10,14 +10,14 @@
 ;    Peter Monks - initial implementation
 ;    Carlo Sciolla - implemented "install" task
 
-(def alfresco-version "4.2.f")
+(def alfresco-version "5.0.d")
 
 (defproject lein-amp "0.7.0-SNAPSHOT"
   :description       "Leiningen plugin for generating Alfresco Module Package (AMP) files."
   :url               "https://github.com/lambdalf/lein-amp"
   :license           {:name "Eclipse Public License"
                       :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version  "2.0.0"
+  :min-lein-version  "2.5.0"
   :repositories      [
                        ["alfresco" "https://artifacts.alfresco.com/nexus/content/groups/public/"]
                      ]
@@ -25,7 +25,7 @@
                        [me.raynes/fs                   "1.4.6"]
                        [org.alfresco/alfresco-mmt      ~alfresco-version]
                        [tk.skuro.alfresco/h2-support   "1.6"]
-                       [com.h2database/h2              "1.4.181"]
+                       [com.h2database/h2              "1.4.190"]
                        [org.eclipse.jetty/jetty-runner "9.2.3.v20140905" :exclusions [org.eclipse.jetty/jetty-jsp]]
                      ]
   :eval-in-leiningen true)
